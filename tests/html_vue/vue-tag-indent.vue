a7177c62de3846dd0d6d07323e34eda799ceca34
kamilic <kamilic.work@outlook.com>
2019-05-27 14:09:50 +0800

    update new test case

---
+++ tests/html_vue/vue-tag-indent.vue
@@ -0,0 +1,17 @@
+<script>
+  console.log('hello');
+</script>
+
+<style lang="scss">
+  footer {
+    background: #00abc9;
+  }
+</style>
+
+<template>
+  <br />
+  <footer>
+    foo
+    <br/>
+  </footer>
+</template>
