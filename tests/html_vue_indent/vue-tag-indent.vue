468552f61cf160a33e7436f2e50c8f7bec315e8d
kamilic <kamilic.work@outlook.com>
2019-06-06 11:43:38 +0800

    move tests to html_vue_indent and some code tweaks

--- tests/html_vue_indent/vue-tag-indent.vue
+++ tests/html_vue_indent/vue-tag-indent.vue
@@ -1,11 +1,16 @@
 <script>
   console.log('hello');
+console.log('hello');
 </script>
 
 <style lang="scss">
   footer {
     background: #00abc9;
   }
+
+footer {
+  background: #00abc9;
+}
 </style>
 
 <template>
